020a506fe84fe54072bc869ee22a5c062d6ee14e
Dominique VASSARD <dominique.vassard@gmail.com>
2018-09-20 13:44:39 +0200

    Initial commit: Chapter 3

---
+++ README.md
@@ -0,0 +1,16 @@
+# Absinthe GraphQL Tutorial  
+
+From Book: `Craft GraphQL APIS in Elixir with Absinthe`  
+
+## Table of contents (and done)  
+[x] Chapter 1: Meet GraphQL  
+[x] Chapter 2: Building a schema  
+[x] Chapter 3: Taking User Input  
+[] Chapter 4: Adding Flexibility  
+[] Chapter 5: Making a Change with Mutation  
+[] Chapter 6: Going Live with Subscriptions  
+[] Chapter 7: Resolution Middleware  
+[] Chapter 8: Securing with Authentication and Authorization  
+[] Chapter 9: Tuning Resolution 
+[] Chapter 10: Driving Phoenix Actions with GraphQL  
+[] Chapter 11: Integrating with the Frontend  
---
+++ lib/plate_slate_web/schema.ex
@@ -0,0 +1,70 @@
+# ---
+# Excerpted from "Craft GraphQL APIs in Elixir with Absinthe",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/wwgraphql for more book information.
+# ---
+defmodule PlateSlateWeb.Schema do
+  use Absinthe.Schema
+  alias PlateSlateWeb.Resolvers
+
+  scalar :date do
+    parse fn input ->
+      with %Absinthe.Blueprint.Input.String{value: value} <- input,
+           {:ok, date} <- Date.from_iso8601(value) do
+        {:ok, date}
+      else
+        _ -> :error
+      end
+    end
+
+    serialize &Date.to_iso8601/1
+  end
+
+  enum :sort_order do
+    value :asc
+    value :desc
+  end
+
+  @desc "Filtering options for the menu item list"
+  input_object :menu_item_filter do
+    @desc "Matching a name"
+    field :name, :string
+
+    @desc "Matching a category name"
+    field :category, :string
+
+    @desc "Matching a tag"
+    field :tag, :string
+
+    @desc "Priced above a value"
+    field :priced_above, :float
+
+    @desc "Priced below a value"
+    field :priced_below, :float
+
+    @desc "Added to the menu before that date"
+    field :added_before, :date
+
+    @desc "Added to the menu after that date"
+    field :added_after, :date
+  end
+
+  query do
+    @desc "The list of available items on the menu"
+    field :menu_items, list_of(:menu_item) do
+      arg :filter, non_null(:menu_item_filter)
+      arg :order, type: :sort_order, default_value: :asc
+      resolve &Resolvers.Menu.menu_items/3
+    end
+  end
+
+  object :menu_item do
+    field :id, :id
+    field :name, :string
+    field :description, :string
+    field :added_on, :date
+  end
+end
